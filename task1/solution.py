ee6c9b93f2ca8eeef60bfe53fa9510eba1d0b25c
AlexanderTahchiev <alexander.tahchiev@gmail.com>
2013-04-29 15:37:39 +0300

    Update solution.py

--- task1/solution.py
+++ task1/solution.py
@@ -1,15 +1,10 @@
-def helper(month, day, all_months_and_sign):
-    if month == 12 and day > 21: 
-        return "Козирог"
-    elif month * 100 + day <= all_months_and_sign[0][0]:  
-        return all_months_and_sign[0][1]
-    else:
-        return helper(month, day, all_months_and_sign[1:])
-
-
 def what_is_my_sign(day, month):
-    return helper(
-        month, day, [(119, "Козирог"), (218, "Водолей"), (320, "Рибa"),
-                     (420, "Овен"), (520, "Телец"), (620, "Близнаци"),
-                     (721, "Рак"), (822, "Лъв"), (922, "Дева"),
-                     (1022, "Везни"), (1121, "Скорпион"), (1221, "Стрелец")])
+    ALL_MONTHE_AND_SIGNS = [(119, "Козирог"), (218, "Водолей"),
+                            (320, "Риби"), (420, "Овен"), (520, "Телец"),
+                            (620, "Близнаци"), (721, "Рак"), (822, "Лъв"),
+                            (922, "Дева"), (1022, "Везни"), (1121, "Скорпион"),
+                            (1221, "Стрелец"), (1231, "Козирог")]
+
+    for i in ALL_MONTHE_AND_SIGNS:
+        if month * 100 + day <= i[0]:
+            return i[1]
