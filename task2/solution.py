fd435decd9ec054fbf46966a61a2582791923dac
AlexanderTahchiev <alexander.tahchiev@gmail.com>
2013-04-29 16:34:18 +0300

    Update solution.py

--- task2/solution.py
+++ task2/solution.py
@@ -1,5 +1,9 @@
+from collections import OrderedDict
+from itertools import count
+
+
 def groupby(function, xs):
-    keys = list(set(map(function, xs)))
+    keys = list(map(function, xs))
     dictonary = {}
     for key in keys:
         values_of_the_key = [x for x in xs if function(x) == key]
@@ -16,3 +20,19 @@ def iterate(func):
 
 def comp(first, second):
     return lambda x: first(second(x))
+
+
+def zip_with(func, *iterables):
+    return (func(*args) for args in zip(*iterables))
+
+
+def cache(func, cache_size):
+    cached_arguments = OrderedDict()
+
+    def func_cached(*args):
+        if args not in cached_arguments:
+            if len(cached_arguments) == cache_size:
+                cached_arguments.popitem(last=False)
+            cached_arguments[args] = func(*args)
+        return cached_arguments[args]
+    return func_cached
