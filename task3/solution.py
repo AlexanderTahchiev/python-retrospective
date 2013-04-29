ee5cd0377eda4243d191cfbdddc51a91bbf41b7e
AlexanderTahchiev <alexander.tahchiev@gmail.com>
2013-04-29 16:54:03 +0300

    Update solution.py

--- task3/solution.py
+++ task3/solution.py
@@ -7,11 +7,11 @@ class Person:
         self.birth_year = birth_year
         self.father = father
         self.mother = mother
+        self._children = []
         if father:
             father.add_child(self)
         if mother:
             mother.add_child(self)
-        self._children = []
 
     def add_brother(self, brother):
         self.brothers.append(brother)
@@ -20,16 +20,18 @@ class Person:
         self.sisters.append(sister)
 
     def get_sisters(self):
-        return list(set(self.father.children('F')) | set(self.mother.children('F')))
+        return list((set(self.father.children('F')) | set(self.mother.children('F'))) - {self})
 
     def get_brothers(self):
-        return list(set(self.father.children('M')) | set(self.mother.children('M')))
+        return list((set(self.father.children('M')) | set(self.mother.children('M'))) - {self})
 
     def add_child(self, child):
         self._children.append(child)
 
-    def children(self, gender):
-        return [child for child in self._children if child.gender == gender]
+    def children(self, gender=None):
+        if gender:
+            return [child for child in self._children if child.gender == gender]
+        return self._children
 
     def is_direct_successor(self, other):
         return other in self._children
